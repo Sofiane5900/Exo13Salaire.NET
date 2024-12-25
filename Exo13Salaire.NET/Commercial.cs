602b39fc01e892f363048f6b07049fcdab6d1923
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-25 22:09:40 +0100

    ajout méthode statique

--- Exo13Salaire.NET/Commercial.cs
+++ Exo13Salaire.NET/Commercial.cs
@@ -10,21 +10,22 @@ namespace Exo13Salaire.NET
         private double _commission;
         private double _comissionPourcentage;
         private List<Commercial> _commercials;
-        private List<Salarie> salaries;
+        private List<Salarie> _salaries;
 
         public int ChiffreAffaire { get => _chiffreAffaire; set => _chiffreAffaire = value; }
         public double Commission { get => _commission; set => _commission = value; }
         public double ComissionPourcentage { get => _comissionPourcentage; set => _comissionPourcentage = value; }
         public  List<Commercial> Commercials { get => _commercials; set => _commercials = value; }
-        public List<Salarie> Salaries { get => _commercials; set => _commercials = value; }
+        public List<Salarie> Salaries { get => _salaries; set => _salaries = value; }
 
 
         // Je déclare un constructeur, "base" appelle tout les attributs de la classe mere Salarie
-        public Commercial(string Matricule, string Categorie, string Service, string Nom, int Salaire, double ComissionPourcentage) : base(Matricule, Categorie, Service, Nom, Salaire)
+        public Commercial(string Matricule, string Categorie, string Service, string Nom, int Salaire, double ComissionPourcentage) 
+        : base(Matricule, Categorie, Service, Nom, Salaire)
         {
             ChiffreAffaire = 7500000;
             _comissionPourcentage = ComissionPourcentage;
-            // Comission = chiffre d'affraire * 3% / 100
+            // Comission = chiffre d'affraire * double / 100
             Commission = ChiffreAffaire * (ComissionPourcentage / 100);
         }
 
@@ -36,11 +37,12 @@ namespace Exo13Salaire.NET
 
         public void AfficherSalaire(Commercial commercial, double Comission)
         {
-            Console.WriteLine($"Le salaire de {commercial.Nom} (matricule: {commercial.Matricule} & categorie: {commercial.Categorie}) est de {commercial.Salaire} incluant une commission de {commercial.Commission} euros.");
+            Console.WriteLine($"Le salaire avec commision de {commercial.Nom} est de {commercial.Salaire + Commission} euros.");
         }
 
 
-        public void IHM()
+        // Une méthode static appartient a la classe, et non pas forcément a une instance de la classe (objet)
+        public static void IHM()
         {
             while (true)
             {
@@ -49,7 +51,6 @@ namespace Exo13Salaire.NET
                 Console.WriteLine("2-- Afficher le salaire des employés");
                 Console.WriteLine("3-- Rechercher un employé");
                 Console.WriteLine("3-- Quitter");
-
                 int choixMenu;
                 bool successChoixMenu = int.TryParse(Console.ReadLine(), out choixMenu);
                 if (!successChoixMenu)
@@ -71,6 +72,39 @@ namespace Exo13Salaire.NET
                         Environment.Exit(0);
                         break;
                     case 1:
+                        Console.WriteLine("=== Ajouter un employé ===\n");
+                        Console.WriteLine("1-- Salarié");
+                        Console.WriteLine("2-- Commerciale");
+                        Console.WriteLine("0-- Retour");
+                        int choixAjout;
+                        bool successChoixAjout = int.TryParse(Console.ReadLine(), out choixAjout);
+                        if (!successChoixAjout)
+                        {
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Erreur, vous n'étes pas autorisé a effectuer cette saisie !");
+                            Console.ResetColor();
+                        } else if (choixAjout == 1) {
+                            Console.Write("Merci de saisir le nom:");
+                            string nomAjout = Console.ReadLine();
+                            Console.Write("Merci de saisir le matricule:");
+                            string matriculeAjout = Console.ReadLine();
+                            Console.Write("Merci de saisir la categorie:");
+                            string categorieAjout = Console.ReadLine();
+                            Console.Write("Merci de saisir le chiffre d'affaire du commerciale:");
+                            int chifreAffaireAjout;
+                            bool successChiffreAffaire = int.TryParse(Console.ReadLine(),out chifreAffaireAjout);
+                            if (!successChiffreAffaire)
+                            {
+                                Console.Clear();
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Erreur, vous n'étes pas autorisé a effectuer cette saisie !");
+                                Console.ResetColor();
+                            }
+                            
+                        }
+
+                        break;
 
                 }
             }
--- Exo13Salaire.NET/Program.cs
+++ Exo13Salaire.NET/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Exo13Salaire.NET;
 
-Commercial Jean = new Commercial("UBM324", "C22M", "Vente", "Jean", 30000, 0.03);
 
-Jean.AfficherSalaire(Jean, 0.03);
+
+Commercial.IHM();
