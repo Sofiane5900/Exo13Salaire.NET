7de5745324a51ed0204c36ed273644943e2d8ca3
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2024-12-25 19:04:01 +0100

    attributs chiffre affaire & commision en %

--- Exo13Salaire.NET/Commercial.cs
+++ Exo13Salaire.NET/Commercial.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using Exo11Salaire.NET;
 
 namespace Exo13Salaire.NET
@@ -10,10 +6,17 @@ namespace Exo13Salaire.NET
     // Je déclare que Commercial hérite de la classe Salarie
     internal class Commercial : Salarie
     {
-        
+        private int _chiffreAffaire;
+        private int _commission;
+
+        public int ChiffreAffaire { get => _chiffreAffaire; set => _chiffreAffaire = value; }
+        public int Commission { get => _commission; set => _commission = value; }
+
         // Je déclare un constructeur, "base" appelle tout les paramétres du constructeur Salarie
-        public Commercial(int Matricule, string Service, string Nom, int Salaire) : base(Matricule, Service, Nom, Salaire)
+        public Commercial(int Matricule, string Service, string Nom, int Salaire, int ChiffreAffaire, int Comission) : base(Matricule, Service, Nom, Salaire)
         {
         }
+
+   
     }
 }
