b0967ee709211ad19490d8389d1fcf7c3c268979
Sufyān <132440690+Sofiane5900@users.noreply.github.com>
2025-01-02 16:42:31 +0100

    IHM dans une autre classe

--- Exo13Salaire.NET/Commercial.cs
+++ Exo13Salaire.NET/Commercial.cs
@@ -1,5 +1,4 @@
-﻿
-using Exo11Salaire.NET;
+﻿using Exo11Salaire.NET;
 
 namespace Exo13Salaire.NET
 {
@@ -9,107 +8,64 @@ namespace Exo13Salaire.NET
         private int _chiffreAffaire;
         private double _commission;
         private double _comissionPourcentage;
-        private List<Commercial> _commercials;
+        private static List<Commercial> _commercials;
         private List<Salarie> _salaries;
 
-        public int ChiffreAffaire { get => _chiffreAffaire; set => _chiffreAffaire = value; }
-        public double Commission { get => _commission; set => _commission = value; }
-        public double ComissionPourcentage { get => _comissionPourcentage; set => _comissionPourcentage = value; }
-        public  List<Commercial> Commercials { get => _commercials; set => _commercials = value; }
-        public List<Salarie> Salaries { get => _salaries; set => _salaries = value; }
-
-
-        // Je déclare un constructeur, "base" appelle tout les attributs de la classe mere Salarie
-        public Commercial(string Matricule, string Categorie, string Service, string Nom, int Salaire, double ComissionPourcentage) 
-        : base(Matricule, Categorie, Service, Nom, Salaire)
+        public int ChiffreAffaire
         {
-            ChiffreAffaire = 7500000;
-            _comissionPourcentage = ComissionPourcentage;
-            // Comission = chiffre d'affraire * double / 100
-            Commission = ChiffreAffaire * (ComissionPourcentage / 100);
+            get => _chiffreAffaire;
+            set => _chiffreAffaire = value;
+        }
+        public double Commission
+        {
+            get => _commission;
+            set => _commission = value;
+        }
+        public double ComissionPourcentage
+        {
+            get => _comissionPourcentage;
+            set => _comissionPourcentage = value;
+        }
+        public static List<Commercial> Commercials
+        {
+            get => _commercials;
+            set => _commercials = value;
+        }
+        public List<Salarie> Salaries
+        {
+            get => _salaries;
+            set => _salaries = value;
         }
 
+        // Je déclare un constructeur, "base" appelle tout les attributs de la classe mere Salarie
+        public Commercial(
+            string Matricule,
+            string Categorie,
+            string Service,
+            string Nom,
+            int Salaire,
+            int ChiffreAffaire
+        )
+            : base(Matricule, Categorie, Service, Nom, Salaire)
+        {
+            _chiffreAffaire = ChiffreAffaire;
+            Commercials = new List<Commercial>();
+        }
 
         public void AfficherSalaire(Salarie salarie)
         {
-            Console.WriteLine($"Le salaire de {salarie.Nom} (matricule: {salarie.Matricule} & categorie: {salarie.Categorie}) est de {salarie.Salaire} euros.");
+            Console.WriteLine(
+                $"Le salaire de {salarie.Nom} (matricule: {salarie.Matricule} & categorie: {salarie.Categorie}) est de {salarie.Salaire} euros."
+            );
         }
 
         public void AfficherSalaire(Commercial commercial, double Comission)
         {
-            Console.WriteLine($"Le salaire avec commision de {commercial.Nom} est de {commercial.Salaire + Commission} euros.");
+            Console.WriteLine(
+                $"Le salaire avec commision de {commercial.Nom} est de {commercial.Salaire + Commission} euros."
+            );
         }
-
 
         // Une méthode static appartient a la classe, et non pas forcément a une instance de la classe (objet)
-        public static void IHM()
-        {
-            while (true)
-            {
-                Console.WriteLine("=== Gestion des employées ===\n");
-                Console.WriteLine("1-- Ajouter un employé");
-                Console.WriteLine("2-- Afficher le salaire des employés");
-                Console.WriteLine("3-- Rechercher un employé");
-                Console.WriteLine("3-- Quitter");
-                Console.Write("Faites votre choix : ");
-                int choixMenu;
-                bool successChoixMenu = int.TryParse(Console.ReadLine(), out choixMenu);
-                if (!successChoixMenu)
-                {
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Erreur, vous n'étes pas autorisé a effectuer cette saisie !");
-                    Console.ResetColor();
-                }
-
-                switch (choixMenu) {
-                    default:
-                        Console.Clear();
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Erreur, vous n'étes pas autorisé a effectuer cette saisie !");
-                        Console.ResetColor();
-                        break;
-                    case 0:
-                        Environment.Exit(0);
-                        break;
-                    case 1:
-                        Console.Clear();
-                        Console.WriteLine("=== Ajouter un employé ===\n");
-                        Console.WriteLine("1-- Salarié");
-                        Console.WriteLine("2-- Commerciale");
-                        Console.WriteLine("0-- Retour");
-                        int choixAjout;
-                        bool successChoixAjout = int.TryParse(Console.ReadLine(), out choixAjout);
-                        if (!successChoixAjout)
-                        {
-                            Console.Clear();
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("Erreur, vous n'étes pas autorisé a effectuer cette saisie !");
-                            Console.ResetColor();
-                        } else if (choixAjout == 1) {
-                            Console.Write("Merci de saisir le nom:");
-                            string nomAjout = Console.ReadLine();
-                            Console.Write("Merci de saisir le matricule:");
-                            string matriculeAjout = Console.ReadLine();
-                            Console.Write("Merci de saisir la categorie:");
-                            string categorieAjout = Console.ReadLine();
-                            Console.Write("Merci de saisir le chiffre d'affaire du commerciale:");
-                            int chifreAffaireAjout;
-                            bool successChiffreAffaire = int.TryParse(Console.ReadLine(),out chifreAffaireAjout);
-                            if (!successChiffreAffaire)
-                            {
-                                Console.Clear();
-                                Console.ForegroundColor = ConsoleColor.Red;
-                                Console.WriteLine("Erreur, vous n'étes pas autorisé a effectuer cette saisie !");
-                                Console.ResetColor();
-                            }
-                            
-                        }
-
-                        break;
-
-                }
-            }
-        }
     }
 }
---
+++ Exo13Salaire.NET/IHM.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exo13Salaire.NET
+{
+    internal class IHM
+    {
+        public static void AfficherIHM()
+        {
+            while (true)
+            {
+                Console.WriteLine("=== Gestion des employées ===\n");
+                Console.WriteLine("1-- Ajouter un employé");
+                Console.WriteLine("2-- Afficher le salaire des employés");
+                Console.WriteLine("3-- Rechercher un employé");
+                Console.WriteLine("4-- Quitter");
+                Console.Write("Faites votre choix : ");
+                if (!int.TryParse(Console.ReadLine(), out int choixMenu))
+                {
+                    AfficherErreur("Erreur, vous n'êtes pas autorisé à effectuer cette saisie !");
+                    continue;
+                }
+
+                switch (choixMenu)
+                {
+                    case 1:
+                        AjouterEmploye();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        AfficherErreur(
+                            "Erreur, vous n'êtes pas autorisé à effectuer cette saisie !"
+                        );
+                        break;
+                }
+            }
+        }
+
+        private static void AjouterEmploye()
+        {
+            Console.Clear();
+            Console.WriteLine("=== Ajouter un employé ===\n");
+            Console.WriteLine("1-- Salarié");
+            Console.WriteLine("2-- Commerciale");
+            Console.WriteLine("0-- Retour");
+            if (!int.TryParse(Console.ReadLine(), out int choixAjout))
+            {
+                AfficherErreur("Erreur, vous n'êtes pas autorisé à effectuer cette saisie !");
+                return;
+            }
+
+            switch (choixAjout)
+            {
+                case 1:
+                    AjouterSalarie();
+                    break;
+                case 2:
+                    AjouterCommercial();
+                    break;
+                case 0:
+                    Console.Clear();
+                    break;
+                default:
+                    AfficherErreur("Erreur, vous n'êtes pas autorisé à effectuer cette saisie !");
+                    break;
+            }
+        }
+
+        private static void AjouterSalarie()
+        {
+            Console.Write("Merci de saisir le nom: ");
+            string nom = Console.ReadLine();
+            Console.Write("Merci de saisir le matricule: ");
+            string matricule = Console.ReadLine();
+            Console.Write("Merci de saisir le service: ");
+            string service = Console.ReadLine();
+            Console.Write("Merci de saisir la catégorie: ");
+            string categorie = Console.ReadLine();
+            Console.Write("Merci de saisir le salaire: ");
+            if (!int.TryParse(Console.ReadLine(), out int salaire))
+            {
+                AfficherErreur("Erreur, veuillez entrer un salaire valide!");
+                return;
+            }
+        }
+
+        private static void AjouterCommercial()
+        {
+            Console.Write("Merci de saisir le nom: ");
+            string nom = Console.ReadLine();
+            Console.Write("Merci de saisir le matricule: ");
+            string matricule = Console.ReadLine();
+            Console.Write("Merci de saisir le service: ");
+            string service = Console.ReadLine();
+            Console.Write("Merci de saisir la catégorie: ");
+            string categorie = Console.ReadLine();
+            Console.Write("Merci de saisir le salaire: ");
+            if (!int.TryParse(Console.ReadLine(), out int salaire))
+            {
+                AfficherErreur("Erreur, veuillez entrer un salaire valide!");
+                return;
+            }
+            Console.Write("Merci de saisir le chiffre d'affaire: ");
+            if (!int.TryParse(Console.ReadLine(), out int chiffreAffaire))
+            {
+                AfficherErreur("Erreur, veuillez entrer un chiffre d'affaire valide!");
+                return;
+            }
+            Console.Write("Merci de saisir la commission: ");
+            if (!int.TryParse(Console.ReadLine(), out int commission))
+            {
+                AfficherErreur("Erreur, veuillez entrer une commission valide!");
+                return;
+            }
+        }
+
+        private static void AfficherErreur(string message)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
